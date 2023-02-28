2a240b5eea59286ac5e947106af72422497d93e8
JegorSt3p <st3pegor@gmail.com>
2023-02-28 18:02:56 +0700

    added Date field to Task class

--- src/main/java/me/tusur/todoapp/pojo/Task.java
+++ src/main/java/me/tusur/todoapp/pojo/Task.java
@@ -1,5 +1,8 @@
 package me.tusur.todoapp.pojo;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 import java.util.UUID;
 
 public class Task {
@@ -7,13 +10,17 @@ public class Task {
 
     private String name;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
     public Task() {
         this.id = UUID.randomUUID().toString();
     }
 
-    public Task(String name) {
+    public Task(String name, Date date) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
+        this.date = date;
     }
 
     public String getId() {
@@ -26,5 +33,13 @@ public class Task {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
