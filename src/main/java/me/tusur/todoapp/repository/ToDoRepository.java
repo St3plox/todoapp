2814b408de0db427f09afd3376513e0edd34708b
JegorSt3p <st3pegor@gmail.com>
2023-02-28 09:40:35 +0700

    added CRUD operations in ToDo Repo

--- src/main/java/me/tusur/todoapp/repository/ToDoRepository.java
+++ src/main/java/me/tusur/todoapp/repository/ToDoRepository.java
@@ -1,9 +1,28 @@
 package me.tusur.todoapp.repository;
 
+import me.tusur.todoapp.pojo.Task;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ToDoRepository {
+    private List<Task> tasks = new ArrayList<>();
+
+    private Task getTask(int index) {
+        return tasks.get(index);
+    }
+
+    private void addTask(Task task){
+        tasks.add(task);
+    }
+
+    private List<Task> getTasks(){
+        return new ArrayList<>(tasks);
+    }
+
+    private void deleteTask(int index){
+        tasks.remove(index);
+    }
 }
