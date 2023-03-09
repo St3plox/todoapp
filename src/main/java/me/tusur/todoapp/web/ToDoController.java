8a763fd8c0f7366fccf9c40a932f9b4ef28e4fd2
JegorSt3p <st3pegor@gmail.com>
2023-03-09 17:48:33 +0700

    index.html

--- src/main/java/me/tusur/todoapp/web/ToDoController.java
+++ src/main/java/me/tusur/todoapp/web/ToDoController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ToDoController {
     @GetMapping
     public String getIndex() {
-        return "sidebar";
+        return "index";
     }
 }
